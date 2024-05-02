6f263d79c7e3a7ad0d64d5a220d74c9838b3c5d0
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-05-02 17:45:36 -0300

    Agregar archivos de proyecto.

---
+++ WebAppiUser/Migrations/20240430145006_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppiUser.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    lastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    rol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ WebAppiUser/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAppiUser.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string firstName { get; set; }
+        public required string lastName { get; set; }
+        public required string email { get; set; }
+        public required string password { get; set; }
+        public string rol { get; set; }
+        public bool isActive { get; set; } = true;
+
+    }
+}
